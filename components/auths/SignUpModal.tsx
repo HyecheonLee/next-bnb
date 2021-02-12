38bf6718d35f31377f5ca212eddbdec7838f228b
이혜천 <rainbow880616@gmail.com>
2021-02-12 23:36:02 +0900

    selector 완성.

--- components/auths/SignUpModal.tsx
+++ components/auths/SignUpModal.tsx
@@ -6,6 +6,9 @@ import PersonIcon from "../../public/static/svg/auth/person.svg";
 import OpenedEyeIcon from "../../public/static/svg/auth/opened_eye.svg";
 import CloseEyeIcon from "../../public/static/svg/auth/closed_eye.svg";
 import Input from "../Input";
+import palette from "../../styles/palette";
+import Selector from "../Selector";
+import { dayList, monthList, yearList } from "../../lib/staticData";
 
 const Container = styled.div`
   width: 568px;
@@ -23,6 +26,37 @@ const Container = styled.div`
     position: relative;
     margin-bottom: 16px;
   }
+
+  .sign-up-birthdate-label {
+    font-size: 16px;
+    font-weight: 600;
+    margin-top: 16px;
+    margin-bottom: 8px;
+  }
+
+  .sign-up-modal-birthday-info {
+    margin-bottom: 16px;
+    color: ${palette.charcoal};
+  }
+
+  .sign-up-modal-birthday-selectors {
+    display: flex;
+    margin-bottom: 24px;
+
+    .sign-up-modal-birthday-month-selector {
+      margin-right: 16px;
+      flex-grow: 1;
+    }
+
+    .sign-up-modal-birthday-day-selector {
+      margin-right: 16px;
+      width: 25%;
+    }
+
+    .sign-up-modal-birthday-year-selector {
+      width: 33.33333%;
+    }
+  }
 `;
 
 interface IProps {
@@ -35,6 +69,9 @@ const SignUpModal: React.FC<IProps> = ({ closeModal }) => {
   const [firstname, setFirstname] = useState("");
   const [password, setPassword] = useState("");
   const [hidePassword, setHidePassword] = useState(true);
+  const [birthYear, setBirthYear] = useState<string | undefined>();
+  const [birthDay, setBirthDay] = useState<string | undefined>();
+  const [birthMonth, setBirthMont] = useState<string | undefined>();
   const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value);
   };
@@ -50,6 +87,16 @@ const SignUpModal: React.FC<IProps> = ({ closeModal }) => {
   const toggleHidePassword = () => {
     setHidePassword(!hidePassword);
   };
+  const onChangeBirthMonth = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthMont(event.target.value);
+  };
+  const onChangeBirthDay = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthDay(event.target.value);
+  };
+  const onChangeBirthYear = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthYear(event.target.value);
+  };
+
   return (
     <Container>
       <CloseXIcon className="modal-close-x-icon" onClick={closeModal} />
@@ -95,6 +142,40 @@ const SignUpModal: React.FC<IProps> = ({ closeModal }) => {
           value={password}
           onChange={onChangePassword}
         />
+      </div>
+      <p className="sign-up-birthdate-label">생일</p>
+      <p className="sign-up-modal-birthday-info">
+        만 18세 이상의 성인만 회원으로 가입할 수 있습니다. 생일은 다른 앙뜨리
+        이용자에게 고개되지 않습니다.
+      </p>
+      <div className="sign-up-modal-birthday-selectors">
+        <div className="sign-up-modal-birthday-month-selector">
+          <Selector
+            options={monthList}
+            disabledOptions={["월"]}
+            defaultValue={"월"}
+            value={birthMonth}
+            onChange={onChangeBirthMonth}
+          />
+        </div>
+        <div className="sign-up-modal-birthday-day-selector">
+          <Selector
+            options={dayList}
+            disabledOptions={["일"]}
+            defaultValue={"일"}
+            value={birthDay}
+            onChange={onChangeBirthDay}
+          />
+        </div>
+        <div className="sign-up-modal-birthday-year-selector">
+          <Selector
+            options={yearList}
+            disabledOptions={["년"]}
+            defaultValue={"년"}
+            value={birthYear}
+            onChange={onChangeBirthYear}
+          />
+        </div>
       </div>
     </Container>
   );
